a37655e18a214e23dadc895a1e313552166e6b7a
Daniil Pokinsokha <pokinsokha@gmail.com>
2024-08-11 10:14:46 +0200

    Delete unused code

--- types/index.ts
+++ types/index.ts
@@ -3,12 +3,3 @@ export interface Position {
   x: number;
   y: number;
 }
-
-export interface Element {
-  type: string;
-  id: number;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
